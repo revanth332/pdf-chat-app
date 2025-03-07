e20f84ac2507efe2221becb6cd31415ef3b93c9b
revathipathilanka347 <revathipathilanka347@gmail.com>
2025-03-07 17:49:24 +0530

    avatar chat completed

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 import streamlit as st
 from PyPDF2 import PdfReader
-import pickle
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
 import os
 from langchain_core.documents import Document
-from langchain_core.vectorstores import InMemoryVectorStore
-# from langchain_pinecone import PineconeVectorStore
-# from pinecone import Pinecone
-
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone,ServerlessSpec
+import time
+from uuid import uuid4
+import re
 
 load_dotenv()
 
-embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=os.getenv("GEMINI_API_KEY"));
-
-# pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
-# index_name = "rag-app-index"
-# index = pc.Index(index_name)
-
-# vector_store = PineconeVectorStore(embedding=embeddings, index=index)
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=os.getenv("GEMINI_API_KEY"))
+pc = Pinecone(api_key=os.getenv("PINECONE_KEY"),ssl_verify=False)
 
 generation_config = {
   "temperature": 1,
@@ -30,6 +25,14 @@ generation_config = {
   "max_output_tokens": 8192,
   "response_mime_type": "text/plain",
 }
+
+def sanitize_index_name(name):
+    """Sanitizes a string to be a valid Pinecone index name."""
+    name = name.lower()  # Convert to lowercase
+    name = re.sub(r"[^a-z0-9-]", "-", name)  # Replace invalid chars with hyphens
+    name = re.sub(r"-+", "-", name) # replace multiples - with a single -
+    name = re.sub(r"^-|-$", "", name) # remove - at the start or end
+    return name
 
 def get_gemini_response(user_prompt: str,context : str, model_name):
     chat_session = None
@@ -43,8 +46,8 @@ def get_gemini_response(user_prompt: str,context : str, model_name):
             chat_session = model.start_chat()
 
         prompt = f"""
-        Assume yourself as a famous scientist name ALbert einstein. Your task is to answer the users question as if you are Einstein.
-        You can refer this context to answer the user query. The context is {context}. Based on this context answer the query is {user_prompt}
+        You are AI assistant that responds to user queries with care and responsibility. Before answering a question you have to refer to the context given.
+        The context is '{context}'. The query is '{user_prompt}'. Now answer the question based on the given context.
         """
         response = chat_session.send_message(prompt)
         return response.text
@@ -57,54 +60,79 @@ def get_gemini_response(user_prompt: str,context : str, model_name):
 # with st.sidebar:
 #     st.title("PDF Chat APP")
 
+def delete_all_indexes():
+    """Deletes all Pinecone indexes."""
+    existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
+    if existing_indexes:
+        st.write("Deleting all existing indexes...")
+        for index_name in existing_indexes:
+            pc.delete_index(index_name)
+            st.write(f"Deleted index: {index_name}")
+    else:
+        st.write("No indexes to delete.")
 
 def main():
     st.header("Chat with Pdf 💬")
     pdf = st.file_uploader("Upload your pdf", type="pdf")
-    
+    if st.button("Delete All Indexes"):
+        delete_all_indexes()
     if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-        
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size = 1000,
-            chunk_overlap = 200,
-            length_function = len
-        )
-
-        chunks = text_splitter.split_text(text=text)
-        store_name = pdf.name[:-4]
-
         vector_store = None
+        index_name = pdf.name[:-4]
+        index_name = sanitize_index_name(index_name)
 
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl","rb") as f:
-                vector_store = pickle.load(f)
-        else:
-            vector_store = InMemoryVectorStore(embeddings)
-            documents = []
-            for index,chunk in enumerate(chunks):
-                document = Document(id=index,page_content=chunk)
-                documents.append(document)
+        existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
+        st.write(existing_indexes)
+
+        if index_name not in existing_indexes:
+            st.write("creating new vector index")
+            pc.create_index(
+                name=index_name,
+                dimension=768,
+                metric="cosine",
+                spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+            )
+
+            while not pc.describe_index(index_name).status["ready"]:
+                time.sleep(1)
             
-            vector_store.add_documents(documents=documents)
+            store_index = pc.Index(index_name)
 
-            with open(f"{store_name}.pkl","wb") as f:
-                pickle.dump(vector_store, f)
+            st.write("creating new vector store")
+            vector_store = PineconeVectorStore(embedding=embeddings, index=store_index)
+            with st.spinner("Processing PDF..."):
+                st.write("Reading pages")
+                pdf_reader = PdfReader(pdf)
+
+                text = ""
+                for page in pdf_reader.pages:
+                    text += page.extract_text()
+                
+                text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size = 1000,
+                    chunk_overlap = 200,
+                    length_function = len
+                )
+
+                chunks = text_splitter.split_text(text=text)
+                documents = []
+                for index,chunk in enumerate(chunks):
+                    document = Document(page_content=chunk)
+                    documents.append(document)
+                
+                uuids = [str(uuid4()) for _ in range(len(documents))]
+                st.write("Adding documents to vector store")
+                vector_store.add_documents(documents=documents,ids=uuids)
+        else:
+            st.write("pulling existing vector store")
+            vector_store = PineconeVectorStore(embedding=embeddings, index=pc.Index(index_name))
         
         query = st.text_input("Ask question about PDF file")
-
-        st.write(query)
-
         if query:
             results = vector_store.similarity_search(query=query,k=1)
             context = ""
             for doc in results:
                 context += doc.page_content
-                # print(f"* {doc.page_content} [{doc.metadata}]")
             
             response = get_gemini_response(query,context,"gemini-1.5-pro")
             st.write(response)
--- pages/Avatar.py
+++ pages/Avatar.py
@@ -1,6 +1,47 @@
 import streamlit as st
 import time
 import numpy as np
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone
+import google.generativeai as genai
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=os.getenv("GEMINI_API_KEY"))
+pc = Pinecone(api_key=os.getenv("PINECONE_KEY"),ssl_verify=False)
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+def get_gemini_response(user_prompt: str,context : str, model_name):
+    chat_session = None
+    try:
+        if chat_session is None:
+            model = genai.GenerativeModel(
+                model_name=model_name,
+                generation_config=generation_config,
+                system_instruction="system instruction"
+            )
+            chat_session = model.start_chat()
+
+        prompt = f"""
+        Assume yourself as a person with name {st.session_state.avatar_selected}. Your task is to answer the users question as if you are {st.session_state.avatar_selected}.
+        You can refer this context to answer the user query. The context is {context}. Based on this context answer the query is {user_prompt}
+        """
+        response = chat_session.send_message(prompt)
+        return response.text
+
+    except Exception as e:
+        st.error(f"Error generating text: {str(e)}")
+        return None
+
 
 st.set_page_config(page_title="Chat with Avatar", page_icon="🤖")
 
@@ -9,6 +50,13 @@ st.write(
     """Enjoy chatting with different famous avatars that you get inspired from most. This helps you to have a good interaction with
     people with real time behaviour. """
 )
+
+personalities = ["Albert Einstein", "MS Dhoni", "Elon Musk"]
+avatar_images = {
+    "Albert Einstein": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/800px-Albert_Einstein_Head.jpg",
+    "MS Dhoni": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/MS_Dhoni_%28Prabhav_%2723_-_RiGI_2023%29.jpg/220px-MS_Dhoni_%28Prabhav_%2723_-_RiGI_2023%29.jpg",
+    "Elon Musk": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/800px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
+}
 
 col1, col2, col3 = st.columns(3)
 
@@ -24,23 +72,30 @@ with col3:
     st.subheader("Elon Musk")
     st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/800px-Elon_Musk_Royal_Society_%28crop2%29.jpg", width=200)
 
+
 if "avatar_selected" not in st.session_state:
     st.session_state.avatar_selected = False
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
+if "vector_store" not in st.session_state:
+    st.session_state.vector_store = None
+
 if col1.button("Chat with Albert Einstein"):
     st.session_state.avatar_selected = "Albert Einstein"
     st.session_state.chat_history = []
+    st.session_state.vector_store = PineconeVectorStore(embedding=embeddings, index=pc.Index("einstein-albert"))
 
 if col2.button("Chat with MS Dhoni"):
     st.session_state.avatar_selected = "MS Dhoni"
     st.session_state.chat_history = []
+    st.session_state.vector_store = PineconeVectorStore(embedding=embeddings, index=pc.Index("dhoni-success-story"))
 
 if col3.button("Chat with Elon Musk"):
     st.session_state.avatar_selected = "Elon Musk"
     st.session_state.chat_history = []
+    st.session_state.vector_store = PineconeVectorStore(embedding=embeddings, index=pc.Index("elon-musk"))
 
 if st.session_state.avatar_selected:
     st.empty()
@@ -55,10 +110,16 @@ if st.session_state.avatar_selected:
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        with st.chat_message("assistant"):
+        with st.chat_message("assistant", avatar=avatar_images[st.session_state.avatar_selected]):
             message_placeholder = st.empty()
             full_response = ""
-            assistant_response = "Hello, I am " + st.session_state.avatar_selected + " How can I help you?"
+            with st.spinner("Thinking..."):
+                results = st.session_state.vector_store.similarity_search(query=prompt,k=1)
+
+                context = ""
+                for doc in results:
+                    context += doc.page_content            
+                assistant_response = get_gemini_response(prompt,context,"gemini-1.5-pro")
             for chunk in assistant_response.split():
                 full_response += chunk + " "
                 time.sleep(0.05)
